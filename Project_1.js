164077a6666cf3eed65c525154bd20493baba0a2
HalidAjaaa <ahdiyathalid22@gmail.com>
2025-06-30 19:37:23 +0800

    Halid

---
+++ Project_1.js
@@ -0,0 +1,9 @@
+// Animasi Smooth Scroll untuk navigasi ke bagian tertentu
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
